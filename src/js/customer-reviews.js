ae59585b08e9f52f89dd8f88d37ea2b3884a05f2
Ivan Hema <hacker19088@gmail.com>
2022-10-09 00:24:04 +0300

    Update

---
+++ src/js/customer-reviews.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+        // spaceBetween: 30,
+        // centeredSlides: true,
+        // autoplay: {
+        //   delay: 2500,
+        //   disableOnInteraction: false
+        // },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        }
+    }
+);
