d17975bd3cbc27b2aea0eab354f83193f53f0f30
DmitriyUshkvarok <82167806+DmitriyUshkvarok@users.noreply.github.com>
2022-10-09 10:06:40 +0000

    Deploying to gh-pages from @ DmitriyUshkvarok/cats-with-glasses@4c12cb585f14b39e111705dd514a5dedaa7540e7 🚀

--- index.26bd82d0.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".gallery-swip",{effect:"fade",grabCursor:!0,autoplay:{delay:3500,disableOnInteraction:!0}});
-//# sourceMappingURL=index.26bd82d0.js.map
---
+++ index.b1e848ad.js
@@ -0,0 +1,2 @@
+new Swiper(".gallery-swip",{effect:"fade",grabCursor:!0,autoplay:{delay:3500,disableOnInteraction:!0}}),new Swiper(".mySwiper",{slidesPerView:1,slidesPerGroup:1,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.b1e848ad.js.map
