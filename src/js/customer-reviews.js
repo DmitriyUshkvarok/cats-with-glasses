1d06f6b8ed98d56793e0d38c1063638cbf0b1041
Ivan Hema <hacker19088@gmail.com>
2022-10-09 11:11:34 +0300

    Update

--- src/js/customer-reviews.js
+++ src/js/customer-reviews.js
@@ -1,10 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
-        // spaceBetween: 30,
-        // centeredSlides: true,
-        // autoplay: {
-        //   delay: 2500,
-        //   disableOnInteraction: false
-        // },
+    // spaceBetween: 30,
+    // centeredSlides: true,
+    // autoplay: {
+    // delay: 2500,
+    // disableOnInteraction: false
+    // },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
