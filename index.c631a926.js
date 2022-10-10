9dc3c98ab8ac7e7a6d7e6c0b36ba5d2699aa7058
DmitriyUshkvarok <82167806+DmitriyUshkvarok@users.noreply.github.com>
2022-10-10 21:04:15 +0000

    Deploying to gh-pages from @ DmitriyUshkvarok/cats-with-glasses@368285240e0cdb60894dd7040c91030e733596a5 🚀

---
+++ index.c631a926.js
@@ -0,0 +1,2 @@
+var panel=document.querySelector(".panel"),openPanelButton=document.querySelector(".btn--servicess"),closePanelButton=panel.querySelector(".button-close");openPanelButton.addEventListener("click",(function(){panel.classList.add("panel--show")})),closePanelButton.addEventListener("click",(function(){panel.classList.remove("panel--show")}));
+//# sourceMappingURL=index.c631a926.js.map
