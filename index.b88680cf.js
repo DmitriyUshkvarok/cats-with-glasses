a59374bbef242a6cb58b608f68e90468a192e84c
DmitriyUshkvarok <82167806+DmitriyUshkvarok@users.noreply.github.com>
2022-10-10 17:54:59 +0000

    Deploying to gh-pages from @ DmitriyUshkvarok/cats-with-glasses@6d7f302fc84a58b3419a88f52611156d736153c6 🚀

---
+++ index.b88680cf.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-franchise-open]"),closeModalBtn:document.querySelector("[data-franchise-modal-close]"),modal:document.querySelector("[data-modal-franchise]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.b88680cf.js.map
