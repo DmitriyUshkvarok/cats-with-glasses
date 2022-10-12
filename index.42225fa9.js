13033b18adcc1f82d5957d689dff52c2c6602eae
DmitriyUshkvarok <82167806+DmitriyUshkvarok@users.noreply.github.com>
2022-10-12 21:49:36 +0000

    Deploying to gh-pages from @ DmitriyUshkvarok/cats-with-glasses@62f1f9b7791bf13bdb1361916529fe4bd97a1f6f 🚀

---
+++ index.42225fa9.js
@@ -0,0 +1,2 @@
+var buttonUp=document.querySelector(".scroll-btn");window.onscroll=function(){window.pageYOffset>200?buttonUp.classList.add("shown"):buttonUp.classList.remove("shown")},buttonUp.onclick=function(){window.scrollTo(0,0)};
+//# sourceMappingURL=index.42225fa9.js.map
