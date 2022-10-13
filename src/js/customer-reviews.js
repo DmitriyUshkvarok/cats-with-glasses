0136cb1183ddfd0870dcd6ca2f9f06c6c979aea6
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-10-13 18:10:18 +0300

    ...

--- src/js/customer-reviews.js
+++ src/js/customer-reviews.js
@@ -2,6 +2,7 @@ var swiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
   slidesPerGroup: 1,
   spaceBetween: 40,
+  loop: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
